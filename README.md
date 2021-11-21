827b7f26fc1ad76679da3b0cc30bfb511cc9b657
Adiamaan Keerthi Matheswaran <mak.adi55@gmail.com>
2021-11-21 13:49:55 -0600

    Added README.md

---
+++ README.md
@@ -0,0 +1,136 @@
+<p align="center">
+<img src="./assets/logo.png" alt="brotground logo"/>
+</p>
+
+# Brotground
+
+This python package is for people who want to learn and explore the wonderful world of brots. Provides an api that allows rapid experimenting and visualization. 
+
+```
+pip install brotground
+```
+
+## Features
+1. _Light weight_, well documented, easy to understand code base
+2. _Extremely modular_, Replace any module with your own definition
+3. _Flexible_, Comes with good defaults but can be overridden
+4. _Zero Effort Setup_, Includes google colab notebooks to start experimenting without any setup
+5. _Minimal Dependency_, Numba for iteration and Matplotlib for rendering
+
+##  Overview
+Brots are generalization of Mandelbrot that takes a generic Mandelbrot equation. This library makes every part of the Mandelbrot equation as a parameter offering extreme flexibility to override or use the default implementation.
+
+>An equation means nothing to me unless it expresses a thought of God. — Srinivasa Ramanujan
+
+A Standard **Mandelbrot** equation,
+<p align="center">
+<img src="https://render.githubusercontent.com/render/math?math=Z_{n %2B 1} = Z_n^2 %2B \mathbb{C}" width=200 height=100 color='grey'>
+</p>
+when implemented and rendered will look like this,
+
+```{python}
+mandel = MandelBrot() # Initialize Mandelbrot
+matplot_renderer = MatplotJupyterRenderer() # Initialize the renderer
+
+mandel.iterate_diverge(max_iterations=25) # Run the iterate diverge loop
+matplot_renderer.plot(mandel, cmap="RdGy") # Plot the results
+```
+<p align="center">
+<img src="./assets/mandelbrot_simple.png">
+</p>
+
+We can further zoom in on the coordinates and iterate-diverge on those coordinates,
+
+```{python}
+mandel.set_boundaries((-0.02, 0.02), (0.780, 0.820)) # Zoom in on the coordinates
+mandel.iterate_diverge(max_iterations=100)
+matplot_renderer.plot(mandel, cmap="plasma")
+```
+
+will render like below,
+<p align="center">
+<img src="./assets/mandelbrot_zoomed.png">
+</p>
+
+
+By changing each part of the equation you can get a range of generation.
+Generalizing the above Mandelbrot equation to k, we get **Multibrot** where,
+
+<p align="center">
+<img src="https://render.githubusercontent.com/render/math?math=Z_{n %2B 1} = Z_n^k %2B \mathbb{C}" width=200 height=100>
+</p>
+
+For a K value of 3 we get a Multibrot rendered like this, 
+
+```{python}
+multi = MultiBrot()
+
+multi.iterate_diverge(max_iterations=15)
+matplot_renderer.plot(multi, cmap="binary")
+```
+
+<p align="center">
+<img src="./assets/multibrot.png">
+</p>
+
+A **Tricorn** brot is expressed as,  
+
+<p align="center">
+<img src="https://render.githubusercontent.com/render/math?math=Z_{n %2B 1} = \overline{Z_n^2} %2B \mathbb{C}" width=200 height=100>
+</p>
+
+```{python}
+tricorn = UserBrot(brot_equation=tricorn_brot_equation)
+
+tricorn.iterate_diverge(max_iterations=15)
+matplot_renderer.plot(tricorn, cmap="RdYlBu")
+```
+
+<p align="center">
+<img src="./assets/tricorn.png">
+</p>
+
+
+A **Burning ship** brot is expressed as,  
+<p align="center">
+<img src="https://render.githubusercontent.com/render/math?math=Z_{n %2B 1} = {|\Re(Z)| %2B 1j %2B |\Im(Z)|}^2 %2B \mathbb{C}" width=500 height=200>
+</p>
+
+```{python}
+burning_ship = UserBrot(brot_equation=burning_ship_brot_equation)
+
+burning_ship.iterate_diverge(max_iterations=15)
+matplot_renderer.plot(burning_ship, cmap="copper")
+```
+
+<p align="center">
+<img src="./assets/burning_ship.png">
+</p>
+
+**JuliaBrot** is an extension to Mandelbrot, in which instead of initializing Z and C as 0 and `complex(i, j)` respectively we initialize Z as `complex(i, j)` and C as a function `f(i, j)` based on the julia set that we want to generate.
+
+For example, to generate a `` julia set we initialize C as `complex(-0.7, 0.35)` and this generates the following,
+
+```
+julia = JuliaBrot(julia_name="frost_fractal")
+julia.iterate_diverge(max_iterations=100)
+
+matplot_renderer.plot(julia, cmap="inferno")
+```
+
+<p align="center">
+<img src="./assets/frost_fractal.png">
+</p>
+
+```
+julia = JuliaBrot(julia_name="galaxiex_fractal")
+julia.iterate_diverge(max_iterations=100)
+
+matplot_renderer.plot(julia, cmap="inferno")
+```
+
+<p align="center">
+<img src="./assets/galaxiex_fractal.png">
+</p>
+
+
--- brots/Brots.py
+++ brots/Brots.py
@@ -1,3 +1,23 @@
+"""Brots module that contains the UserBrot, MultiBrot, MandelBrot
+and JuliaBrot definitions
+
+UserBrot: This is the base brot that is used to create all other brots.
+This will default to a mandelbrot.
+
+MultiBrot: This is similar to UserBrot, but instead of Z^2 + C,
+it will use a parameter as the exponent, making it Z^k + C.
+Everything else remains the same.
+
+ManderlBrot: This is the same as UserBrot and inherits from Multibrot.
+Added as a class separately so that it is evident for API usage.
+
+JuliaBrot: This uses the mandelbrot equation but changes the Z and C
+initialization. Usually Z is initialized as 0, but in JuliaSet, it is 
+initialized as complex number of (i, j). Similarly, C is initialized as
+a complex number of (i, j), whereas in Julia set it is set constant based on
+the type of JuliaSet you are trying to generate.
+
+"""
 from typing import Callable
 
 import numba as nb
@@ -22,14 +42,45 @@ class UserBrot(BrotBase):
         c_initialization: Callable = None,
         z_initialization: Callable = None,
     ):
+        """UserBrot constructor
+
+        Args:
+            x_range (RangeType, optional): [description]. Defaults to (-2, 2).
+
+            y_range (RangeType, optional): [description]. Defaults to (-2, 2).
+
+            resolution (int, optional): [description]. Defaults to 1000.
+
+            dtype ([type], optional): [description]. Defaults to np.uint16. Directly proportional
+                to the run time of convergence.
+
+            All the equations default to  is done this way instead of passing the function as default argument to
+            get around the shortcoming of passing numba compiled function as argument.
+
+            core (Callable, optional): [description]. The iterate_diverge logic.
+                If not given the numba compiled logic will be taken as default.
+
+            brot_equation (Callable, optional): Equation that governs the brot generation. Defaults to
+                Mandelbrot equation, which is Z^2 + C.
+
+            divergence_condition (Callable, optional): Divergence condition that marks which points
+                have diverged. Defaults to abs(z) < 2.
+
+            c_initialization (Callable, optional): C Initializaiton, defaulting to complex(i, j)
+
+            z_initialization (Callable, optional): Z Initialization, defaulting to 0.
+
+        Returns:
+            [type]: [description]
+        """
 
         if brot_equation is None:
 
-            def multibrot_equation(z: complex, c: complex):
+            def mandelbrot_equation(z: complex, c: complex):
                 return z ** 2 + c
 
             brot_equation = nb.jit(nopython=True, nogil=True, fastmath=True)(
-                multibrot_equation
+                mandelbrot_equation
             )
 
         self._brot_equation = brot_equation
@@ -74,6 +125,14 @@ class UserBrot(BrotBase):
         super(UserBrot, self).__init__(x_range, y_range, resolution, dtype)
 
     def iterate_diverge(self, threshold: int = 2, max_iterations: int = 25):
+        """Iterate and diverge function that calls the core function.
+
+        Args:
+            threshold (int, optional): Defaults to 2. That controls the divergence condition.
+            max_iterations (int, optional): Defaults to 25. That controls the max iteration.
+
+            Both threshold and max_iteration are used to control the convergence.
+        """
         self._array = self._core(
             self._x,
             self._y,
@@ -108,6 +167,19 @@ class MultiBrot(UserBrot):
         dtype=np.uint16,
         exponent: float = 3,
     ):
+        """
+            This is similar to UserBrot, but instead of Z^2 + C,
+            it will use a parameter as the exponent, making it Z^k + C.
+            Everything else remains the same.
+
+        Args:
+            x_range (RangeType, optional): [description]. Defaults to (-2, 2).
+            y_range (RangeType, optional): [description]. Defaults to (-2, 2).
+            resolution (int, optional): [description]. Defaults to 1000.
+            dtype ([type], optional): [description]. Defaults to np.uint16.
+            exponent (float, optional): [description]. Defaults to 3.
+        """
+
         def multibrot_equation(z: complex, c: complex, exponent: float = exponent):
             return z ** exponent + c
 
@@ -127,6 +199,15 @@ class MandelBrot(MultiBrot):
         resolution: int = 1000,
         dtype=np.uint16,
     ):
+        """This is the same as UserBrot and inherits from Multibrot.
+        Added as a class separately so that it is evident for API usage.
+
+        Args:
+            x_range (RangeType, optional): [description]. Defaults to (-2, 1).
+            y_range (RangeType, optional): [description]. Defaults to (-1.5, 1.5).
+            resolution (int, optional): [description]. Defaults to 1000.
+            dtype ([type], optional): [description]. Defaults to np.uint16.
+        """
         super(MandelBrot, self).__init__(x_range, y_range, resolution, dtype, 2)
 
 
@@ -140,6 +221,22 @@ class JuliaBrot(UserBrot):
         julia_definition: Callable = None,
         julia_name: str = None,
     ):
+        """
+        This uses the mandelbrot equation but changes the Z and C
+        initialization. Usually Z is initialized as 0, but in JuliaSet, it is
+        initialized as complex number of (i, j). Similarly, C is initialized as
+        a complex number of (i, j), whereas in Julia set it is set constant based on
+        the type of JuliaSet you are trying to generate.
+
+        Args:
+            x_range (RangeType, optional): [description]. Defaults to (-2, 2).
+            y_range (RangeType, optional): [description]. Defaults to (-2, 2).
+            resolution (int, optional): [description]. Defaults to 1000.
+            dtype ([type], optional): [description]. Defaults to np.uint16.
+            julia_definition (Callable, optional): [description]. Defaults to None.
+            julia_name (str, optional): [description]. Defaults to None.
+        """
+
         def z_initialization(i, j):
             return complex(i, j)
 
