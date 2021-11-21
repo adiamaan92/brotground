16dfa63a1a4c892e8d7e739d7365572bba3a5a7a
Adiamaan Keerthi Matheswaran <mak.adi55@gmail.com>
2021-11-21 16:57:52 -0600

    Simplifying folder structure

--- brotground/__init__.py
+++ brotground/__init__.py
@@ -1,3 +1,4 @@
 from .BrotBase import BrotBase, RangeType
 from .Brots import JuliaBrot, MandelBrot, MultiBrot, UserBrot
 from .resources import burning_ship_brot_equation, tricorn_brot_equation
+from .static_renderers import MatplotJupyterRenderer
