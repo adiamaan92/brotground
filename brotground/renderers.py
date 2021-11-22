d96c578e39e1eb52cfc566d9e8049d12bc79b4b1
Adiamaan Keerthi Matheswaran <mak.adi55@gmail.com>
2021-11-21 22:36:11 -0600

    moving renderer and republishing

--- brotground/renderers.py
+++ brotground/renderers.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
 
-class MatplotJupyterRenderer:
-    def __init__(self):
-        pass
+class StaticRenderer:
+    def __init__(self, figsize=(10, 10)):
+        self.figsize = figsize
 
     def plot(self, brot, figsize=(10, 10), cmap="plasma"):
         plt.figure(figsize=(figsize))
--- brotground/resources.py
+++ brotground/resources.py
@@ -3,7 +3,6 @@
 This provides additional brots and julia sets for the project.
 This is in no means exhaustive, but it is a good start.
 """
-import matplotlib.pyplot as plt
 import numba as nb
 import numpy as np
 
@@ -38,13 +37,3 @@ quadratic_julia_set = dict(
     groovy_fractal=complex(-0.75, 0.15),
     frost_fractal=complex(-0.7, 0.35),
 )
-
-
-class StaticRenderer:
-    def __init__(self, figsize=(10, 10)):
-        self.figsize = figsize
-
-    def plot(self, brot, figsize=(10, 10), cmap="plasma"):
-        plt.figure(figsize=(figsize))
-        plt.imshow(brot.fetch_array(), cmap=cmap)
-        plt.axis("off")
