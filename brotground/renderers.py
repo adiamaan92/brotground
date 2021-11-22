e1a3dc278fec0e61852ff4fa143ab6cfba46b6cf
Adiamaan Keerthi Matheswaran <mak.adi55@gmail.com>
2021-11-21 21:46:15 -0600

    removing imports

--- brotground/renderers.py
+++ brotground/renderers.py
@@ -1,13 +1,11 @@
 import matplotlib.pyplot as plt
 
-from brotground.BrotBase import BrotBase
-
 
 class MatplotJupyterRenderer:
     def __init__(self):
         pass
 
-    def plot(self, brot: BrotBase, figsize=(10, 10), cmap="plasma"):
+    def plot(self, brot, figsize=(10, 10), cmap="plasma"):
         plt.figure(figsize=(figsize))
         plt.imshow(brot.fetch_array(), cmap=cmap)
         plt.axis("off")
